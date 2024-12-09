e687be1ed65779b0f0cc60620e72fe1a0f5af963
Cydo <cydoentis@gmail.com>
2024-12-09 14:30:05 -0500

    Added a file to hold my cookie names as constants, to avoid magic strings

---
+++ CollabParty.Application/Common/Constants/CookieNames.cs
@@ -0,0 +1,8 @@
+namespace CollabParty.Application.Common.Constants;
+
+public class CookieNames
+{
+    public const string AccessToken = "QB-ACCESS-TOKEN";
+    public const string RefreshToken = "QB-REFRESH-TOKEN";
+    public const string CsrfToken = "QB-CSRF-TOKEN";
+}
