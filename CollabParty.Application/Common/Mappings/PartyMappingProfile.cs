99a722cd98de34893db388895ec255924a7732dc
Cydo <cydoentis@gmail.com>
2024-12-18 21:50:27 -0500

    Made sure the party includes the party leader in the party member list.

--- CollabParty.Application/Common/Mappings/PartyMappingProfile.cs
+++ CollabParty.Application/Common/Mappings/PartyMappingProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CollabParty.Application.Common.Dtos.Party;
 using CollabParty.Domain.Entities;
+using CollabParty.Domain.Enums;
 
 namespace CollabParty.Application.Common.Mappings;
 
@@ -9,9 +10,14 @@ public class PartyMappingProfile : Profile
     public PartyMappingProfile()
     {
         CreateMap<Party, PartyDto>()
+            .ForMember(dest => dest.PartyMembers, opt => opt
+                .MapFrom(src =>
+                    src.PartyMembers.Where(pm => pm.Role == UserRole.Leader)
+                        .Union(src.PartyMembers.Where(pm => pm.Role != UserRole.Leader).Take(3))))
             .ForMember(dest => dest.TotalPartyMembers, opt => opt.MapFrom(src => src.PartyMembers.Count))
             .ForMember(dest => dest.TotalQuests, opt => opt.MapFrom(src => src.Quests.Count))
-            .ForMember(dest => dest.CompletedQuests, opt => opt.MapFrom(src => src.Quests.Count(q => q.IsCompleted)))
+            .ForMember(dest => dest.CompletedQuests,
+                opt => opt.MapFrom(src => src.Quests.Count(q => q.IsCompleted)))
             .ForMember(dest => dest.PastDueQuests,
                 opt => opt.MapFrom(src => src.Quests.Count(q => q.DueDate < DateTime.UtcNow)));
         CreateMap<CreatePartyDto, Party>();
