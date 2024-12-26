af71d0da961d821f9633448ac9600ca18ac381bd
Cydo <cydoentis@gmail.com>
2024-12-26 17:26:49 -0500

    Added AddCommentRequestDto.cs

--- CollabParty.Application/Common/Dtos/QuestComments/AddCommentRequestDto.cs
+++ CollabParty.Application/Common/Dtos/QuestComments/AddCommentRequestDto.cs
@@ -2,5 +2,7 @@ namespace CollabParty.Application.Common.Dtos.QuestComments;
 
 public class AddCommentRequestDto
 {
-    
+    public int QuestId { get; set; }
+    public string UserId { get; set; }
+    public string Content { get; set; }
 }
