36c099c5a4bc5b8de875087159afe22a058c2a62
Cydo <cydoentis@gmail.com>
2025-01-04 18:23:12 -0500

    Updated QuestAssignmentService.cs to not use try catches and allow the global error handler to catch all errors

--- CollabParty.Application/Services/Implementations/QuestAssignmentService.cs
+++ CollabParty.Application/Services/Implementations/QuestAssignmentService.cs
@@ -24,118 +24,94 @@ public class QuestAssignmentService : IQuestAssignmentService
 
     public async Task<AssignPartyMembersResponseDto> AssignPartyMembersToQuest(int questId, string[] partyMemberIds)
     {
-        try
+        var validUserIds = await _unitOfWork.User.GetAllAsync(u => partyMemberIds.Contains(u.Id));
+
+        if (validUserIds.Count() != partyMemberIds.Length)
         {
-            var validUserIds = await _unitOfWork.User.GetAllAsync(u => partyMemberIds.Contains(u.Id));
-
-            if (validUserIds.Count() != partyMemberIds.Length)
-            { 
-                throw new ResourceCreationException("Some party members are invalid or do not exist.");
-            }
-
-            var partyMembers = partyMemberIds.Select(partyMemberId => new QuestAssignment()
-            {
-                QuestId = questId,
-                UserId = partyMemberId,
-                AssignedAt = DateTime.UtcNow,
-                IsCompleted = false,
-            }).ToList();
-
-            foreach (var partyMember in partyMembers)
-            {
-                await _unitOfWork.QuestAssignment.CreateAsync(partyMember);
-            }
-
-            return new AssignPartyMembersResponseDto()
-            {
-                Message = "Party members assigned to quest",
-                QuestId = questId
-            };
+            throw new ResourceCreationException("Some party members are invalid or do not exist.");
         }
-        catch (Exception ex)
+
+        var partyMembers = partyMemberIds.Select(partyMemberId => new QuestAssignment()
         {
-            _logger.LogError(ex, "Failed to assign members to quest.");
-            throw new ResourceCreationException("An error occurred while assigning members to the quest.");
+            QuestId = questId,
+            UserId = partyMemberId,
+            AssignedAt = DateTime.UtcNow,
+            IsCompleted = false,
+        }).ToList();
+
+        foreach (var partyMember in partyMembers)
+        {
+            await _unitOfWork.QuestAssignment.CreateAsync(partyMember);
         }
+
+        return new AssignPartyMembersResponseDto()
+        {
+            Message = "Party members assigned to quest",
+            QuestId = questId
+        };
     }
 
 
     public async Task<UpdateAssignedPartyMembersResponseDto> UpdateQuestAssignments(int questId,
         string[] updatedPartyMemberIds)
     {
-        try
-        {
-            var existingAssignments = await _unitOfWork.QuestAssignment.GetAllAsync(qa => qa.QuestId == questId);
+        var existingAssignments = await _unitOfWork.QuestAssignment.GetAllAsync(qa => qa.QuestId == questId);
 
-            if (EntityUtility.EntityIsNull(existingAssignments))
-                throw new NotFoundException("Quest assignments not found");
+        if (EntityUtility.EntityIsNull(existingAssignments))
+            throw new NotFoundException("Quest assignments not found");
 
-            var assignmentsToDelete = existingAssignments
-                .Where(assignment => !updatedPartyMemberIds.Contains(assignment.UserId))
-                .ToList();
+        var assignmentsToDelete = existingAssignments
+            .Where(assignment => !updatedPartyMemberIds.Contains(assignment.UserId))
+            .ToList();
 
-            var assignmentsToAdd = updatedPartyMemberIds
-                .Where(userId => !existingAssignments.Any(assignment => assignment.UserId == userId))
-                .Select(userId => new QuestAssignment
-                {
-                    QuestId = questId,
-                    UserId = userId,
-                    AssignedAt = DateTime.UtcNow,
-                    IsCompleted = false
-                })
-                .ToList();
-
-            foreach (var assignment in assignmentsToDelete)
+        var assignmentsToAdd = updatedPartyMemberIds
+            .Where(userId => !existingAssignments.Any(assignment => assignment.UserId == userId))
+            .Select(userId => new QuestAssignment
             {
-                await _unitOfWork.QuestAssignment.RemoveAsync(assignment);
-            }
+                QuestId = questId,
+                UserId = userId,
+                AssignedAt = DateTime.UtcNow,
+                IsCompleted = false
+            })
+            .ToList();
 
-            foreach (var newAssignment in assignmentsToAdd)
-            {
-                await _unitOfWork.QuestAssignment.CreateAsync(newAssignment);
-            }
-
-            return new UpdateAssignedPartyMembersResponseDto()
-                { Message = "Quest assignments updated", QuestId = questId, IsSuccess = true };
-        }
-        catch (Exception ex)
+        foreach (var assignment in assignmentsToDelete)
         {
-            _logger.LogError(ex, "Failed to update quest assignments.");
-            throw new ResourceModificationException("An error occured while updating quest assignments.");
+            await _unitOfWork.QuestAssignment.RemoveAsync(assignment);
         }
+
+        foreach (var newAssignment in assignmentsToAdd)
+        {
+            await _unitOfWork.QuestAssignment.CreateAsync(newAssignment);
+        }
+
+        return new UpdateAssignedPartyMembersResponseDto()
+            { Message = "Quest assignments updated", QuestId = questId, IsSuccess = true };
     }
 
     public async Task<DeleteAssignedPartyMembersResponseDto> DeleteQuestAssignments(int questId,
         string[] partyMemberIdsToDelete)
     {
-        try
+        var existingAssignments = await _unitOfWork.QuestAssignment.GetAllAsync(qa => qa.QuestId == questId);
+
+        if (existingAssignments == null || !existingAssignments.Any())
+            throw new NotFoundException("Quest assignments not found");
+
+
+        var assignmentsToDelete = existingAssignments
+            .Where(assignment => partyMemberIdsToDelete.Contains(assignment.UserId))
+            .ToList();
+
+        if (!assignmentsToDelete.Any())
+            throw new NotFoundException("No quest assignments for deletion could be found.");
+
+
+        foreach (var assignment in assignmentsToDelete)
         {
-            var existingAssignments = await _unitOfWork.QuestAssignment.GetAllAsync(qa => qa.QuestId == questId);
-
-            if (existingAssignments == null || !existingAssignments.Any())
-                throw new NotFoundException("Quest assignments not found");
-
-
-            var assignmentsToDelete = existingAssignments
-                .Where(assignment => partyMemberIdsToDelete.Contains(assignment.UserId))
-                .ToList();
-
-            if (!assignmentsToDelete.Any())
-                throw new NotFoundException("No quest assignments for deletion could be found.");
-
-
-            foreach (var assignment in assignmentsToDelete)
-            {
-                await _unitOfWork.QuestAssignment.RemoveAsync(assignment);
-            }
-
-            return new DeleteAssignedPartyMembersResponseDto()
-                { Message = "Quest assignments deleted", QuestId = questId };
+            await _unitOfWork.QuestAssignment.RemoveAsync(assignment);
         }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Failed to delete quest assignments.");
-            throw new ResourceModificationException("An error occured while deleting quest assignments.");
-        }
+
+        return new DeleteAssignedPartyMembersResponseDto()
+            { Message = "Quest assignments deleted", QuestId = questId };
     }
 }
